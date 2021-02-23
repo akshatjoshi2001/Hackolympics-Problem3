9d2856aa5950512ea55575a5ae648bed673966c0
Akshat Joshi <akshatjoshi375@gmail.com>
2021-02-23 14:16:58 +0530

    Made some changes

--- client.js
+++ client.js
@@ -1,12 +1,31 @@
+/*
+
+   This is the local client which will listen for commands from our public endpoint.
+   
+
+*/
+
+const ENDPOINT = "http://hackolymp.herokuapp.com/"; // The URL of our endpoint
 const {exec} = require("child_process")
 const io = require("socket.io-client");
 
-const socket = io("http://hackolymp.herokuapp.com/");
+const socket = io(ENDPOINT);
 
+
+// Ones command is recieved, send that command for execution to our mini SDK.
 socket.on('command',(data)=>{
-    exec("node cmd.js "+data.command,(err,out)=>{
 
-        console.log(out)
+    sdkCommand = "node cmd.js "+data.command // Can be replaced with any other SDK like gcloud/kubectl
+    exec(sdkCommand,(err,out)=>{
 
+        if(!err)
+            {
+             console.log(out)  // Print the output of the command if no errors are found.
+            }
+            else
+            {
+                console.log("Error:" + err) // In case of error, show the error
+            }
+    
     })
 })
--- cmd.js
+++ cmd.js
@@ -1,31 +1,47 @@
+/*
 
+    This is our mini SDK. It can run predefined commands. If the entered command is not predefined, it will run that command on the shell/terminal
+*/
+
+
+
+const {exec} = require("child_process")
 
 processCommand(process.argv[2],process.argv.slice(3,process.argv.length))
 
 
-
+// Function to process each command recieved
 function processCommand(command,args)
 {
     
     if(command.toLowerCase()=="print")
     {
-       string = ""
-        for(i in args)
-        {
-            
-                string += args[i]+" "
-        
-        }
+
+        // Building the  string to be printed on screen
+       string = args.join(" ")
+       
         console.log(string)
 
     }
-    else if(command.toLowerCase()=="run")
+
+    
+   
+    else  // If the command is not predefined it will be running the asked command on shell
     {
-        console.log("running " + args)
-    }
-    else
-    {
-        console.log("Unrecognised command")
+        // Building the command string to be executed.
+        str = command + " " + args.join(" ")
+        console.log("Command asked to be run: " + str)
+        exec(str,(err,out)=>{
+            if(!err)
+            {
+             console.log(out)  // Print the output of the command if no errors are found.
+            }
+            else
+            {
+                console.log("Error:" + err) // In case of error, show the error
+            }
+    
+        })
     }
 
 }
